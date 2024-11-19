6e94d288323bcaf6ff7555e3a1f37923b8114738
Paula Sanchez <paula.sanchez1031@gmail.com>
2024-11-19 12:55:02 -0500

    feat(landing): add elements to get functional landing

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, Text, Box } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Paula!";
 const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const bio2 = "specialized in React";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -15,7 +13,38 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={{ base: 2, lg: 6 }} align="center">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        gap="4"
+        mb="8"
+      >
+        <Avatar
+          name="Paula Sanchez"
+          src="https://raw.githubusercontent.com/psanchezp31/hosted-assets/refs/heads/main/PXL_20240926_205345829.jpg"
+          size="xl"
+        />
+        <Heading as="h1" size="md">
+          {greeting}
+        </Heading>
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Text fontSize="28px" fontWeight="semibold">
+          {bio1}
+        </Text>
+        <Text fontSize="28px" fontWeight="semibold">
+          {bio2}
+        </Text>
+      </Box>
+    </VStack>
   </FullScreenSection>
 );
 
